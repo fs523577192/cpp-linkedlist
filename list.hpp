5a560562f19e4b7644b633e00a9a48dc6f3995d4
Firas <448445223@qq.com>
2024-01-04 23:52:11 +0800

    Initial commit

---
+++ list.hpp
@@ -0,0 +1,13 @@
+#ifndef LIST_H
+#define LIST_H
+
+template<typename T> class List {
+public:
+    virtual unsigned size() = 0;
+    virtual T get(unsigned index) = 0;
+    virtual T set(unsigned index, T value) = 0;
+    virtual void add(unsigned index, T value) = 0;
+    //virtual T remove(unsigned index) = 0;
+};
+
+#endif // LIST_H
